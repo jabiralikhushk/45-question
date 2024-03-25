2d36073bf825bcee2c3bcab7c30c795b094efde3
Jabir ali <163226601+jabiralikhushk@users.noreply.github.com>
2024-03-25 07:44:50 +0500

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,46 +1,135 @@
-// //Task 21
-// //Think of something you could store a typescript object.write a program 
-// //Think create object containing these items.
-let person: {name: string, fname: string, age: number} = {name: "jabir", fname: "male", age:18};
-console.log(person);
+// Task 24
+// More conditional tests: you dont have to limit the number of tests to create to 10. if you want to try more compression,
+// Write more tests. more at least one true and one false result for each of the following 
+// Task for equality and unequality for strings 
+// let name_1 : string = "sidra"
+// let name_2 : string = "akram"
+// let name_3 : string = "miss sidra"l
+// if (name_1 == name_3){
+//     console.log("names are equal")
+// } else {
+//     console.log("names are not equal")
+// }
+// if (name_1 != name_2){
+//     console.log("names are equal")
+// } else {
+//     console.log("names are not equal")
+// }
 
-// //Task 22
-// //International error:if you have not receive in array index error in one of your
-// //program yet, try to make one happen. change in index in one of your program 
-// //produce in index error. make sure you correct the error before closing the program.
-const days : string [] = ['friday','saturday','sunday','monday','tuesday','wednesday','thursday'];
-console.log(days{7});
-console.log(days[6]);
+// let age_1 : number = 18
+// let age_2 : number = 27
+// if (age_1 == 18){
+//     console.log("eligible for vote")
+// }
+// if (age_2 == 27){
+//         console.log("eligible for vote in older category")
+//     }
+// if (age_1 <= age_2){
+//          console.log("younger")
+//      }
+//      if (age_2 <= age_1){
+//              console.log("older")
+        //  }
+        //  if (age_1 >= age_2){
+        //     console.log("younger")
+        // }
+        // if (age_1 <= age_2){
+        //     console.log("older")
+        // }
 
-// //Task 23
-let car = "subaru";
-console.log("is car == 'subaru'? pridict true");
-console.log(car == 'subaru');
+        // if ("age_1 == 18 && age_2 == 27"){
+        //     console.log("person is eligible for vote ")
+        // }
+        // if ("age_1 == 18 || age_2 != 27") {
+        //     console.log("person is eligible not for vote ")
+        // }
+        // let country : string [] = ["pakistan","japan","china"]
+        // if (country.includes("pakistan")){
+        //     console.log("pakistan is in country list")
+        // }
+        // if (country.includes("japan")){
+        //         console.log("india is not include in an array")
+        //      }
 
-console.log("is car !='hunda city'? pridict true");
-console.log(car != 'hunda city');
+    //     // Task 25
+    //    let alien_colour : string = "green"
+    //     if (alien_colour == "green")
+    //     console.log('you earn 7 points')
+    // let alien_colour : string = "red"
+    // if (alien_colour == "red")
+    //   console.log('you earn 10 points')
 
-console.log("is car == 'Subaru'? pridict false")
-console.log(car =='Subaru')
+    // Task 26
+    // let alien_colour = "green"
+    //     if (alien_colour == "green"){
+    //      console.log('player just earn 5 points for shotting the alien ')  
+    //     } else {
+    //         console.log("player just earned 10 points ")
+    //     }
+        // let alien_colour = "red"
+        // if (alien_colour == "green"){
+        //  console.log('player just earn 5 points for shotting the alien ')  
+        // } else {
+        //     console.log("player just earned 10 points ")
+        // }
 
-console.log("is car == 'SUBARU'? pridict false")
-console.log(car =='SUBARU')
+        //Task 27
+        // let alien_colour : string = "yellow"
+        // if (alien_colour == "green"){
+        //     console.log("5 points")
+        // } else if (alien_colour == "yellow"){
+        //     console.log("10 points")
+        // } else  {
+        //     console.log("15 points")
+         // }
 
-console.log("is car.length == 6? predict true")
-console.log(car.length ==6)
+      //Task 28 ........2,4,11,20,65,older
+// let age : number = 65
+// if (age < 2) {
+//     console .log ("you are a baby")
+// } else if (age < 4) {
+//     console.log("you are a toddler")
+// }else if (age < 11) {
+//     console.log("you are a kid")
+// } else if (age < 20) {
+//     console.log("you are a teenager")
+// } else if (age < 65) {
+//     console.log("you are a adult")
+// } else {
+//     console.log("you are a older")
+// }
 
-console.log("is car.length l== 10? predict true")
-console.log(car.length [1== 10])
+// //Task 29 you realy like banana
+// let favourite_fruit : string [] = ["apple" ,"orange" ,"grapes",]
+// if (favourite_fruit.includes("apple")) {
+//     console.log("apple")
+// }
+//  if (favourite_fruit.includes("grapes")) {
+//     console.log("you realy like bananas")
+// } 
+// if (favourite_fruit. includes("orange")) {
+//     console.log("orange")
+// }
+// if (favourite_fruit.includes("watermellon")) {
+// console.log("you realy like banana") 
+// } 
+ 
+// if (favourite_fruit .includes("grapes")) {
+//     console.log("grapes")
+//  }
+ 
+// Task 30
+// hello admain : make a array five are more usernames,including the name "admain". imagine your writing code will print 
+// greating to each user after the log into the website. loop throw the array and print a greating to each user
+// if user name "admain" print a special greating, such as hello admain, would you like to see a status report 
+// otherwise, print a generic greeting, such as hello eric, thank you for logging again
+//  let users: string[] = ["admain", "eric", "ali"]
+// for(let user of users) {
+//     if (user === "admain")
+//     console.log("hello admain, would you like to see a status report?")
+// }
+// {
+//     console.log ('hello $(user),thank you loging in again')
+// }
 
-console.log("is 10 > 5 ? predict false")
-console.log(10 > 45 )
-
-console.log("is 3 <= 2? predict false")
-console.log(3 <= 2 )
-
-console.log("is 72 > 63? predict false")
-console.log(72 > 63 )
-
-console.log("is car == 'subaru' && car.length == 6? predict true ")
-console.log('car == subaru' && 'car.length == 6')
 
