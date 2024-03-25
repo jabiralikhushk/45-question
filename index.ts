f7206c1a26b2e6c9462a31d1de543153cc190d06
Jabir ali <163226601+jabiralikhushk@users.noreply.github.com>
2024-03-25 07:45:48 +0500

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,135 +1,69 @@
-// Task 24
-// More conditional tests: you dont have to limit the number of tests to create to 10. if you want to try more compression,
-// Write more tests. more at least one true and one false result for each of the following 
-// Task for equality and unequality for strings 
-// let name_1 : string = "sidra"
-// let name_2 : string = "akram"
-// let name_3 : string = "miss sidra"l
-// if (name_1 == name_3){
-//     console.log("names are equal")
-// } else {
-//     console.log("names are not equal")
+// Task 31
+// no users : add an if test to exercise 28 to make sore the list of user has not empty
+// . if the list is empty, print the message we need to find some users !
+// . remove all the user name from your array, and make sure the correct message is printed 
+//   let users :string [] = ["eric", "ali", "ahmed", "admain"];
+//   if (users.length === 0) {
+//     console.log("we need to find some users!");
 // }
-// if (name_1 != name_2){
-//     console.log("names are equal")
-// } else {
-//     console.log("names are not equal")
-// }
-
-// let age_1 : number = 18
-// let age_2 : number = 27
-// if (age_1 == 18){
-//     console.log("eligible for vote")
-// }
-// if (age_2 == 27){
-//         console.log("eligible for vote in older category")
-//     }
-// if (age_1 <= age_2){
-//          console.log("younger")
+// else {
+//     for (let user of users) {
+//      if (user === "admain") {
+//         console.log("hello admain, would you like to see a status report?")
+//      }  else {
+//         console.log(`hello ${user}, thank you for loging in again`)
 //      }
-//      if (age_2 <= age_1){
-//              console.log("older")
-        //  }
-        //  if (age_1 >= age_2){
-        //     console.log("younger")
-        // }
-        // if (age_1 <= age_2){
-        //     console.log("older")
-        // }
-
-        // if ("age_1 == 18 && age_2 == 27"){
-        //     console.log("person is eligible for vote ")
-        // }
-        // if ("age_1 == 18 || age_2 != 27") {
-        //     console.log("person is eligible not for vote ")
-        // }
-        // let country : string [] = ["pakistan","japan","china"]
-        // if (country.includes("pakistan")){
-        //     console.log("pakistan is in country list")
-        // }
-        // if (country.includes("japan")){
-        //         console.log("india is not include in an array")
-        //      }
-
-    //     // Task 25
-    //    let alien_colour : string = "green"
-    //     if (alien_colour == "green")
-    //     console.log('you earn 7 points')
-    // let alien_colour : string = "red"
-    // if (alien_colour == "red")
-    //   console.log('you earn 10 points')
-
-    // Task 26
-    // let alien_colour = "green"
-    //     if (alien_colour == "green"){
-    //      console.log('player just earn 5 points for shotting the alien ')  
-    //     } else {
-    //         console.log("player just earned 10 points ")
-    //     }
-        // let alien_colour = "red"
-        // if (alien_colour == "green"){
-        //  console.log('player just earn 5 points for shotting the alien ')  
-        // } else {
-        //     console.log("player just earned 10 points ")
-        // }
-
-        //Task 27
-        // let alien_colour : string = "yellow"
-        // if (alien_colour == "green"){
-        //     console.log("5 points")
-        // } else if (alien_colour == "yellow"){
-        //     console.log("10 points")
-        // } else  {
-        //     console.log("15 points")
-         // }
-
-      //Task 28 ........2,4,11,20,65,older
-// let age : number = 65
-// if (age < 2) {
-//     console .log ("you are a baby")
-// } else if (age < 4) {
-//     console.log("you are a toddler")
-// }else if (age < 11) {
-//     console.log("you are a kid")
-// } else if (age < 20) {
-//     console.log("you are a teenager")
-// } else if (age < 65) {
-//     console.log("you are a adult")
-// } else {
-//     console.log("you are a older")
+//     } 
+    
 // }
 
-// //Task 29 you realy like banana
-// let favourite_fruit : string [] = ["apple" ,"orange" ,"grapes",]
-// if (favourite_fruit.includes("apple")) {
-//     console.log("apple")
-// }
-//  if (favourite_fruit.includes("grapes")) {
-//     console.log("you realy like bananas")
-// } 
-// if (favourite_fruit. includes("orange")) {
-//     console.log("orange")
-// }
-// if (favourite_fruit.includes("watermellon")) {
-// console.log("you realy like banana") 
-// } 
- 
-// if (favourite_fruit .includes("grapes")) {
-//     console.log("grapes")
+//  users = []
+//  if (users.length === 0) {
+//      console.log("we need to find some user!")
 //  }
- 
-// Task 30
-// hello admain : make a array five are more usernames,including the name "admain". imagine your writing code will print 
-// greating to each user after the log into the website. loop throw the array and print a greating to each user
-// if user name "admain" print a special greating, such as hello admain, would you like to see a status report 
-// otherwise, print a generic greeting, such as hello eric, thank you for logging again
-//  let users: string[] = ["admain", "eric", "ali"]
-// for(let user of users) {
-//     if (user === "admain")
-//     console.log("hello admain, would you like to see a status report?")
-// }
+
+//Task 32
+// checking the username the following to create a program that simulates how website insure that everyone has unique username.
+// . make a list of five or more username called current_users.
+// . make another list of  five user name called new_users. make sure one are two of new username are also in the current_users 
+// list 
+
+// . loop through the new_user list to see if each new username have already been used. if it has, print a message that the person 
+// will need to enter a new username. if a user name has not been used, print a message saying that the username is available
+// let current_users : string [] = ["admain","ali","ahmed"]
+// let new_users : string [] = ["admain","ali","shahmeer","noor"]
+// let current_users_lower : string [] = current_users.map(user => user.toLowerCase())
+
+// for (let new_user of new_users) {
+//  if (current_users_lower.includes (new_user.toLowerCase()))
 // {
-//     console.log ('hello $(user),thank you loging in again')
-// }
+//  console.log(`sorry ${new_users}, that name is taken`) 
+//  } else {
+//     console.log(`yes ${new_user}, is still in available list `)
+//  }
+//      }
+
+// Task 33
+// ordinal Numbers: ordinal number indicate their position in array, such as 1st or 2nd. most ordinal numbers end in the, except
+// 1,2, and 3. 
+//store the number 1 through 9 in a array.
+// loop through the array. use an if.else chain inside the loop to parent the proper ordinal ending for each number. your output
+//should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", in each result should be a seprate line 
+
+// let numbers : number [] = [1,2,3,4,5,6,7,8,9]
+// for (let number of numbers) {
+//     if (number === 1) {
+//         console.log(`${numbers}st`)
+//     } else if (number === 2) {
+//         console.log(`${numbers}nd`) 
+//         } else if (number === 3) {
+//             console.log(`${numbers}rd`)
+//         } else {
+//             console.log(`${numbers}th`)
+//         }
+        
+        
+//         }
+    
 
 
